4e96111fa65d71ce81fbbab968a484014ce14fc4
ivanakulov <ia3175622@gmail.com>
2024-06-18 21:12:12 +0500

    initial commit

---
+++ form_ajax.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  $("form").submit(function (event) {
+    let formData = {
+      user_name: $("#user_name").val(),
+      user_second_name: $("#user_second_name").val(),
+      user_last_name: $("#user_last_name").val(),
+    };
+
+    $.ajax({
+      type: "POST",
+      url: "dadata.php",
+      data: formData,
+      dataType: "json",
+      encode: true,
+    }).done(function (result) {
+      console.log(result);
+    for (let value of result) {
+
+        $("#result").html(
+            '<p>Полное имя ' + value.result + '</p><p>В лице ' + value.result_genitive + '</p><p>Кому ' + value.result_dative + '</p><p>Согласовано ' + value.result_ablative + '</p><p>Имя ' +  value.name  + '</p><p> Фамилия ' + value.surname + '</p><p> Отчество ' + value.patronymic
+          );
+    }
+
+    });
+
+    event.preventDefault();
+  });
+});
